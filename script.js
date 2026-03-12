1ec0e337434beb03b8bcff22a9891cb5fef3c325
PedrooHanry <pedrohanry.ficr@gmail.com>
2026-03-12 13:27:49 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+// Cotação da moeda
+
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
+    const hasCharactersRegex = /\D+/g
+    // console.log(amount.value)
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+// Capturando o evento de submit do formulário
+form.onsubmit = function(event) {
+    event.preventDefault
+
+    // console.log(currency.value)
+
+    switch (currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol){
+    try {
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+        let total = String((amount * price)).replace(".",",")
+
+        // Verificação para ver se o usuario está colocando somente números.
+        if(isNaN(total)){
+            return alert("Por favor, digite o valor corretamente para converter.")
+        }
+
+        // Exibir resultado total
+        result.textContent = `${total} Reais`
+
+        footer.classList.add("show-result")
+    } catch (error) {
+        footer.classList.remove("show-result")
+
+        console.log(error)
+        alert("Não foi possível converter. Tente novamente mais tarde.")
+    }
+}
+
+// Função para formatar a moeda em Real Brasileiro.
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR"), {
+        style: "currency",
+        currency: "BRL",
+    }
+}
